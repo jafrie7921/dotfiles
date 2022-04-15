aa2c42284f5c9405359ecc0a3ee3a4e0fff2f68f
jafrie7921 <>
2022-04-15 16:33:43 +0200

    Create vimrc if not exists

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Checking for updates..." && git pull && echo "Copying files..." && mv ~/.vimrc ~/.vimrc.bak && cp .vimrc ~/.vimrc && echo "Done :)"
+echo "Checking for updates..." \
+	&& git pull \
+	&& echo "Copying files..." \
+	&& touch ~/.vimrc \
+	&& mv ~/.vimrc ~/.vimrc.bak \
+	&& cp .vimrc ~/.vimrc \
+	&& echo "Done :)"
 
